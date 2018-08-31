79a0ec2b8e21743f8fcbc58fecd1b460facb5b0f
hengyi.zhang <hengyi.zhang@uama.com.cn>
2018-08-31 12:21:46 +0800

    new content

---
+++ UITableViewDemo/TestTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TestTableViewCell.swift
+//  UITableViewDemo
+//
+//  Created by zhanghengyi on 2018/8/31.
+//  Copyright © 2018 zhanghengyi. All rights reserved.
+//
+
+import UIKit
+
+class TestTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ UITableViewDemo/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  UITableViewDemo
+//
+//  Created by zhanghengyi on 2018/8/31.
+//  Copyright © 2018 zhanghengyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.register(UINib.init(nibName: "TestTableViewCell", bundle: nil), forCellReuseIdentifier: "TestTableViewCell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.tableFooterView = UIView.init()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TestTableViewCell", for: indexPath)
+        
+        return UITableViewCell.init()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3;
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100;
+    }
+}
+
